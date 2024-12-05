793b1034a890157366d08ed1d6801528a1a8fe85
Mohammod Hanif <honifnoob@gmail.com>
2024-12-04 20:41:46 -0800

    new project

---
+++ script.js
@@ -0,0 +1,7 @@
+let tasks = [
+  { id: 1, text: "task1", status: true },
+  { id: 2, text: "text", status: false },
+  { id: 3, text: "text", status: true },
+];
+
+const parent = document.getElementById("parent");
